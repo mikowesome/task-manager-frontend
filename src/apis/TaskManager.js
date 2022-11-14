f52cdb0474777eb3afe6cd173422c90cce3276bc
Miko <jm_mapilisan@yahoo.com>
2022-11-14 23:52:58 +0800

    Update TaskManager Api Link

--- src/apis/TaskManager.js
+++ src/apis/TaskManager.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/v1/tasks"
+    baseURL: "https://mikowesome-task-manager-api.onrender.com/api/v1/tasks"
 })
