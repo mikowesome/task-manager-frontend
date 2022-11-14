e976c04d6db8dc2c4cf201df5b884f255573a9bd
Miko <jm_mapilisan@yahoo.com>
2022-11-14 23:27:13 +0800

    Initialize project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from "./components/Navbar";
+import TaskForm from "./components/TaskForm";
+import TaskInfo from "./components/TaskInfo";
+import TaskList from "./components/TaskList";
+import { useTasksContext } from "./hooks/useTasksContext";
+import loadingImage from "./assets/images/loader.gif"
 
 function App() {
+  const { isLoading } = useTasksContext()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="pages">
+        <TaskForm />
+        <TaskInfo />
+        {
+          isLoading && <div className="loader">
+            <img src={loadingImage} alt="loading gif" />
+          </div>
+        }
+        <TaskList />
+      </div>
+      <ToastContainer />
     </div>
   );
 }
