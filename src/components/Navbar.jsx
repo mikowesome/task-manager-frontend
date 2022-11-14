de071fdaaae9756b03d801521a0df27580a7b3d5
Miko <jm_mapilisan@yahoo.com>
2022-11-14 23:25:36 +0800

    Add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <header>
+        <div className='container'>
+            <h1>Task Manager</h1>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
